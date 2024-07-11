a259f1d2f315e0c107a43e623afd3f5976fb6777
Sergei Nagaev <nagaev.sv@gmail.com>
2024-07-11 09:04:17 +0200

    Claude 3.5 Sonnet model support added.

--- chibi/services/providers/anthropic.py
+++ chibi/services/providers/anthropic.py
@@ -56,6 +56,7 @@ class Anthropic(Provider):
         all_models = [
             "claude-3-opus-20240229",
             "claude-3-sonnet-20240229",
+            "claude-3-5-sonnet-20240620",
             "claude-3-haiku-20240307",
             "claude-2.1'",
             "claude-2.0",
