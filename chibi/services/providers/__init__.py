56396cfd04c1b64d450763e9809a09897de35ed3
Sergei <nagaev.sv@gmail.com>
2026-09-08 14:19:02 +0200

    Feat: Sambanova added

--- chibi/services/providers/__init__.py
+++ chibi/services/providers/__init__.py
@@ -21,6 +21,7 @@ from chibi.services.providers.novita import Novita
 from chibi.services.providers.open_router import OpenRouter
 from chibi.services.providers.openai import OpenAI
 from chibi.services.providers.provider import RegisteredProviders
+from chibi.services.providers.sambanova import SambaNova
 from chibi.services.providers.suno import Suno
 from chibi.services.providers.together import Together
 from chibi.services.providers.xiaomi import Xiaomi
