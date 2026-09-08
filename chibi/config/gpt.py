c2325e5c6f6cdfa42f0f4efa42fb1d1c30710774
Sergei <nagaev.sv@gmail.com>
2026-09-08 14:19:02 +0200

    Feat: Sambanova added

--- chibi/config/gpt.py
+++ chibi/config/gpt.py
@@ -31,6 +31,7 @@ class GPTSettings(BaseSettings):
     fireworks_key: str | None = Field(alias="FIREWORKS_API_KEY", default=None)
     nebius_key: str | None = Field(alias="NEBIUS_API_KEY", default=None)
     baseten_key: str | None = Field(alias="BASETEN_API_KEY", default=None)
+    sambanova_key: str | None = Field(alias="SAMBANOVA_API_KEY", default=None)
     openai_key: str | None = Field(alias="OPENAI_API_KEY", default=None)
     suno_key: str | None = Field(alias="SUNO_API_ORG_API_KEY", default=None)
     elevenlabs_api_key: str | None = Field(alias="ELEVEN_LABS_API_KEY", default=None)
